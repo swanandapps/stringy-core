53fe5c9bef8719ae471e6d42bea3747c7f1a3b01
swanandapps <beingswanand@gmail.com>
2023-11-26 15:08:35 +0530

    filenames standardized, readme cooler

--- index.js
+++ index.js
@@ -1,30 +1,35 @@
 // Importing functions from category files
-import * as caseManipulation from "./lib/caseManipulation";
-import * as whitespaceHandling from "./lib/whitespaceHandling";
-import * as DateAndTimeFormatting from "./lib/DateAndTimeFormatting";
-import * as maskingAndSecurity from "./lib/maskingAndSecurity";
-import * as dataExtractionAndAnalysis from "./lib/dataExtractionAndAnalysis";
-import * as specializedStringOperations from "./lib/specializedStringOperations";
-import * as stringAnalysisAndValidation from "./lib/stringAnalysisAndValidation";
-import * as textTransformationsAndUtilities from "./lib/textTransformationsAndUtilities";
+import * as textCaseManipulation from "./lib/textCaseManipulation";
+import * as textCleaning from "./lib/textCleaning";
+import * as textFormatting from "./lib/textFormatting";
+import * as textMaskingAndSecurity from "./lib/textMaskingAndSecurity";
+import * as textMetadataAndExtraction from "./lib/textMetadataAndExtraction";
+import * as textSpecializedOperations from "./lib/textSpecializedOperations";
+import * as textAnalysisAndValidation from "./lib/textAnalysisAndValidation";
+import * as textTransformations from "./lib/textTransformations";
+import * as textGeneration from "./lib/textGeneration";
 
 // Combining all functions into a single object
 const _s = {
-  ...caseManipulation,
-  ...whitespaceHandling,
-  ...maskingAndSecurity,
-  ...dataExtractionAndAnalysis,
-  ...specializedStringOperations,
-  ...stringAnalysisAndValidation,
-  ...textTransformationsAndUtilities,
+  ...textCaseManipulation,
+  ...textCleaning,
+  ...textFormatting,
+  ...textMaskingAndSecurity,
+  ...textMetadataAndExtraction,
+  ...textSpecializedOperations,
+  ...textAnalysisAndValidation,
+  ...textTransformations,
+  ...textGeneration,
 };
 
 // Exporting individual functions and the _s object
-export * from "./lib/caseManipulation";
-export * from "./lib/whitespaceHandling";
-export * from "./lib/maskingAndSecurity";
-export * from "./lib/dataExtractionAndAnalysis";
-export * from "./lib/specializedStringOperations";
-export * from "./lib/StringAnalysisAndValidation";
-export * from "./lib/textTransformationsAndUtilities";
+export * from "./lib/textCaseManipulation";
+export * from "./lib/textCleaning";
+export * from "./lib/textFormatting";
+export * from "./lib/textMaskingAndSecurity";
+export * from "./lib/textMetadataAndExtraction";
+export * from "./lib/textSpecializedOperations";
+export * from "./lib/textAnalysisAndValidation";
+export * from "./lib/textTransformations";
+export * from "./lib/textGeneration";
 export { _s };
