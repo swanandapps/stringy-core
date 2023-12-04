63d4977408331fbdd735369f658312026ab996cc
Hitendra Mali <39190812+h1t3ndr4@users.noreply.github.com>
2023-12-04 10:47:27 +0530

    Update index.test.js - test cases added for newly added functions

--- tests/index.test.js
+++ tests/index.test.js
@@ -9,6 +9,19 @@ import {
   invertCase,
   extractHashtags,
   formatTime,
+  extractURLs,
+  extractEmails,
+  extractMentions,
+  extractParenthesizedContent,
+  extractQuotedText,
+  extractHTMLTags,
+  extractDates,
+  extractPhoneNumbers,
+  extractIPv4Addresses,
+  extractIPv6Addresses,
+  extractFilePaths,
+  extractDomainNames,
+  extractJSONStrings
 } from "../index";
 
 describe("capitalize", () => {
@@ -174,3 +187,160 @@ describe("formatTime", () => {
 
     // More test for formatTime...
 });
+
+describe("extractURLs", () => {
+  test("extracts URLs from a string containing multiple URLs", () => {
+    expect(extractURLs("Visit https://example.com for more information.")).toEqual([
+      "https://example.com",
+    ]);
+  });
+
+  test("returns an empty array when the string contains no URLs", () => {
+    expect(extractURLs("No URLs here.")).toEqual([]);
+  });
+});
+
+describe("extractEmails", () => {
+  test("extracts email addresses from a string containing multiple emails", () => {
+    expect(extractEmails("Contact us at info@example.com or support@test.org.")).toEqual([
+      "info@example.com", "support@test.org",
+    ]);
+  });
+
+  test("returns an empty array when the string contains no email addresses", () => {
+    expect(extractEmails("No emails here.")).toEqual([]);
+  });
+});
+
+describe("extractMentions", () => {
+  test("extracts mentions from a string containing mentions", () => {
+    expect(extractMentions("This is a @mention example.")).toEqual([
+      "@mention",
+    ]);
+  });
+
+  test("returns an empty array when the string contains no mentions", () => {
+    expect(extractMentions("No mentions here.")).toEqual([]);
+  });
+});
+
+describe("extractParenthesizedContent", () => {
+  test("extracts content within parentheses from a string", () => {
+    expect(extractParenthesizedContent("This is (some content) within parentheses.")).toEqual([
+      "(some content)",
+    ]);
+  });
+
+  test("returns an empty array when the string contains no content within parentheses", () => {
+    expect(extractParenthesizedContent("No parentheses here.")).toEqual([]);
+  });
+});
+
+describe("extractQuotedText", () => {
+  test("extracts quoted text from a string containing quotes", () => {
+    expect(extractQuotedText('She said, "This is a quoted text."')).toEqual([
+      '"This is a quoted text."',
+    ]);
+  });
+
+  test("returns an empty array when the string contains no quoted text", () => {
+    expect(extractQuotedText("No quotes here.")).toEqual([]);
+  });
+});
+
+describe("extractHTMLTags", () => {
+  test("extracts HTML tags from a string containing HTML", () => {
+    expect(extractHTMLTags('<p>This is a paragraph.</p> <a href="#">Click here</a>')).toEqual([
+      '<p>', '</p>', '<a href="#">', '</a>',
+    ]);
+  });
+
+  test("returns an empty array when the string contains no HTML tags", () => {
+    expect(extractHTMLTags("No HTML tags here.")).toEqual([]);
+  });
+});
+
+describe("extractDates", () => {
+  test("extracts dates from a string containing dates", () => {
+    expect(extractDates("Meeting on 12/31/2022. Event: 01-15-2023")).toEqual([
+      '12/31/2022', '01-15-2023',
+    ]);
+  });
+
+  test("returns an empty array when the string contains no dates", () => {
+    expect(extractDates("No dates here.")).toEqual([]);
+  });
+});
+
+describe("extractPhoneNumbers", () => {
+  test("extracts phone numbers from a string containing phone numbers", () => {
+    expect(extractPhoneNumbers("Contact us at 123-456-7890 or +1 (987) 654-3210.")).toEqual([
+      '123-456-7890', '+1 (987) 654-3210',
+    ]);
+  });
+
+  test("returns an empty array when the string contains no phone numbers", () => {
+    expect(extractPhoneNumbers("No phone numbers here.")).toEqual([]);
+  });
+});
+
+describe("extractIPv4Addresses", () => {
+  test("extracts IPv4 addresses from a string containing IPv4 addresses", () => {
+    expect(extractIPv4Addresses("IP addresses: 192.168.1.1 and 10.0.0.1")).toEqual([
+      '192.168.1.1', '10.0.0.1',
+    ]);
+  });
+
+  test("returns an empty array when the string contains no IPv4 addresses", () => {
+    expect(extractIPv4Addresses("No IPv4 addresses here.")).toEqual([]);
+  });
+});
+
+describe("extractIPv6Addresses", () => {
+  test("extracts IPv6 addresses from a string containing IPv6 addresses", () => {
+    expect(extractIPv6Addresses("IPv6 addresses: 2001:0db8:85a3:0000:0000:8a2e:0370:7334 and fe80::1")).toEqual([
+      '2001:0db8:85a3:0000:0000:8a2e:0370:7334', 'fe80::1',
+    ]);
+  });
+
+  test("returns an empty array when the string contains no IPv6 addresses", () => {
+    expect(extractIPv6Addresses("No IPv6 addresses here.")).toEqual([]);
+  });
+});
+
+describe("extractFilePaths", () => {
+  test("extracts file paths from a string containing file paths", () => {
+    expect(extractFilePaths("File paths: /path/to/file1.txt and C:\\Documents\\file2.docx")).toEqual([
+      '/path/to/file1.txt', 'C:\\Documents\\file2.docx',
+    ]);
+  });
+
+  test("returns an empty array when the string contains no file paths", () => {
+    expect(extractFilePaths("No file paths here.")).toEqual([]);
+  });
+});
+
+describe("extractDomainNames", () => {
+  test("extracts domain names from a string containing domain names", () => {
+    expect(extractDomainNames("Visit https://www.example.com for more info.")).toEqual([
+      'www.example.com',
+    ]);
+  });
+
+  test("returns an empty array when the string contains no domain names", () => {
+    expect(extractDomainNames("No domain names here.")).toEqual([]);
+  });
+});
+
+describe("extractJSONStrings", () => {
+  test("extracts JSON strings from a string containing JSON strings", () => {
+    expect(extractJSONStrings('{"key": "value", "nested": {"inner": 42}}')).toEqual([
+      '{"key": "value", "nested": {"inner": 42}}',
+    ]);
+  });
+
+  test("returns an empty array when the string contains no JSON strings", () => {
+    expect(extractJSONStrings("No JSON strings here.")).toEqual([]);
+  });
+});
+
